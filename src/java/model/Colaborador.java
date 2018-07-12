a71c5cafe224e13a108c3cc58f6a9d68d0a9730b
gabrielveigalima <gabrielveigalima@icloud.com>
2018-07-12 09:08:50 -0300

    Cadastro de colaborador parte 1

--- src/java/model/Colaborador.java
+++ src/java/model/Colaborador.java
@@ -15,4 +15,28 @@ public class Colaborador {
     String nome;
     long matricula;
     Date dataInicioCentroCusto;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public long getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(long matricula) {
+        this.matricula = matricula;
+    }
+
+    public Date getDataInicioCentroCusto() {
+        return dataInicioCentroCusto;
+    }
+
+    public void setDataInicioCentroCusto(Date dataInicioCentroCusto) {
+        this.dataInicioCentroCusto = dataInicioCentroCusto;
+    }
 }
