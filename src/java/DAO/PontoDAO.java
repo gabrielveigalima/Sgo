0d76a6f9543252dc1d1f1819b1de4b5fd4bd3463
gabrielveigalima <gabrielveigalima@icloud.com>
2018-09-05 10:57:41 -0300

    Verifica se o ponto já foi lançado

--- src/java/DAO/PontoDAO.java
+++ src/java/DAO/PontoDAO.java
@@ -7,11 +7,11 @@ package DAO;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
 import model.CentroCusto;
 import model.Colaborador;
 import model.Equipamento;
@@ -26,6 +26,7 @@ public class PontoDAO {
     
     Statement stmt;
     PreparedStatement ps;
+    ResultSet rs;
     
     Ponto p = new Ponto();
     Colaborador c = new Colaborador();
@@ -36,22 +37,25 @@ public class PontoDAO {
      
     public String cadastrarPonto(Ponto p, CentroCusto cc, Colaborador c, String id) throws SQLException{
         String r = "";
-        String query = ("insert into tblHoras (id_colaborador,"
-                + "id_centro_custo,"
-                + "id_equipamento,"
-                + "date,"
-                + "hora_inicio,"
-                + "hora_fim,"
-                + "hora_extra_incio,"
-                + "hora_extra_fim,"
-                + "id_user_create,date_time_create,id_user_update,date_time_update) "+
-                "values(?,?,?,?,?,?,?,?,?,?,?,?)"); 
-        String timeStamp = new SimpleDateFormat("yyyyMMdd HH:mm:ss").format(Calendar.getInstance().getTime());
-        
-            //Cadastra usuario
-            String ano = new SimpleDateFormat("yyyy").format(Calendar.getInstance().getTime());
+        String ano = new SimpleDateFormat("yyyy").format(Calendar.getInstance().getTime());
+        if(!verificaPonto(c.getId(),ano+p.getDate()+"01")){//Verifica se o ponto já foi lançado 
+            r = "<div class='bg-danger'><h4 class='text-center' style='padding-top:10px; padding-bottom:5px'>Erro ao Cadastrar ponto: Ponto já registrado.</h4></div><br>"; 
+            String query = ("insert into tblHoras (id_colaborador,"
+             + "id_centro_custo,"
+             + "id_equipamento,"
+             + "date,"
+             + "hora_inicio,"
+             + "hora_fim,"
+             + "hora_extra_incio,"
+             + "hora_extra_fim,"
+             + "id_user_create,date_time_create,id_user_update,date_time_update) "+
+             "values(?,?,?,?,?,?,?,?,?,?,?,?)"); 
+            String timeStamp = new SimpleDateFormat("yyyyMMdd HH:mm:ss").format(Calendar.getInstance().getTime());
             String diazero = "";
-            for(int i=1;i<32;i++){
+
+            //Cadastra pontos
+
+            for(int i=1;i<32;i++){//Cadastras todos os 30 ou 31 dias 
                 if(i<10){
                     diazero = "0";
                 }else{
@@ -67,9 +71,6 @@ public class PontoDAO {
                 }else{
                     ps.setString(3, null);
                 }
-                System.out.print("ANOOOOO: "+ano+p.getDate()+diazero+i);
-                //String data = new SimpleDateFormat("yyyyMMdd").format(ano+p.getDate()+diazero+i);
-                //Date d = (Date) ano+p.getDate()+diazero+i;
                 ps.setString(4, ano+p.getDate()+diazero+i);
                 ps.setString(5, p.getHoraInicio());
                 ps.setString(6, p.getHoraFim()); 
@@ -91,6 +92,33 @@ public class PontoDAO {
                     }
                 }
             }
+        }else{
+            r = "<div class='bg-danger'><h4 class='text-center' style='padding-top:10px; padding-bottom:5px'>Erro ao Cadastrar ponto: Ponto já lançado.</h4></div><br>";
+        }
+         
         return r;
+    }
+    
+    public boolean verificaPonto(String id, String data) throws SQLException{
+        boolean r = false;
+        ps = con.prepareStatement("SELECT id FROM tblHoras WHERE id_colaborador=? AND date=?");
+        ps.setString(1, id);
+        ps.setString(2, data);
+        System.out.println("id: "+id);
+        System.out.println("date: "+data);
+        try {
+            rs = ps.executeQuery();
+            if(rs.next()){
+                r = true;
+                System.out.println("SIMM: ");
+            }else{
+                r = false; 
+                System.out.println("NAOO: ");
+            }
+        }catch (Exception ex) {
+            System.err.println("Erro ao selecionar: " + ex.toString());
+            r = false;
+        }
+        return r; 
     }
 }
